8471e3a9d74361f1b6f20b6282ee77b1e8e962e6
lovamialyramanandafy <ramalomi@gmail.com>
2025-08-01 21:35:16 -0400

    initial commit

---
+++ src/main/java/com/fpmc/gestion_culte/GestionCulteApplication.java
@@ -0,0 +1,13 @@
+package com.fpmc.gestion_culte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionCulteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionCulteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fpmc/gestion_culte/GestionCulteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fpmc.gestion_culte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionCulteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
